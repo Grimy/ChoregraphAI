b6cab36f68dbb3bcce54300423d9434c003fddbc
Victor Adam <victor.adam@engie.com>
2016-05-25 14:00:30 +0200

    New line-of-sight

--- los.c
+++ los.c
@@ -3,28 +3,19 @@ static int los(double y, double x) {
 	double dx = player->x - x;
 	int cy = (int) (y + .5);
 	int cx = (int) (x + .5);
-	// double error = -(cy - y) * dx + (cx - x) * dy;
-	double error = 0;
 	if (dx * (cx - x) > 0 &&
 		dy * (cy - y) > 0 &&
 		has_wall(cy, cx))
 		return 0;
 	while (cy != player->y || cx != player->x) {
-		double err_y = ABS(error - SIGN(dy) * dx);
-		double err_x = ABS(error + SIGN(dx) * dy);
+		double err_y = ABS((cx - x) * dy - (cy + SIGN(dy) - y) * dx);
+		double err_x = ABS((cx + SIGN(dx) - x) * dy - (cy - y) * dx);
 		int old_cx = cx;
-		if (err_x < err_y + .001) {
-			cx += SIGN(dx);
-			if (has_wall(cy, cx))
+		if (err_x < err_y + .001 && has_wall(cy, cx += SIGN(dx)))
+			return 0;
+		if (err_y < err_x + .001)
+			if (has_wall(cy += SIGN(dy), cx) || has_wall(cy, old_cx))
 				return 0;
-			error += SIGN(dx) * dy;
-		}
-		if (err_y < err_x + .001) {
-			cy += SIGN(dy);
-			if (has_wall(cy, cx) || has_wall(cy, old_cx))
-				return 0;
-			error -= SIGN(dy) * dx;
-		}
 	}
 	return 1;
 }
