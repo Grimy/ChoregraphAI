e385730b3d9084f4515e3f4e3250f498a006b82c
Victor Adam <victor@drawall.cc>
2016-07-09 09:54:30 +0200

    More optimization

--- solve.c
+++ solve.c
@@ -6,23 +6,32 @@
 #include "main.c"
 
 #define MAX_LENGTH    32
-#define MAX_SCORE     64
 #define MAX_BACKTRACK 6
+#define BUF_SIZE      (1 << 16)
 
 typedef struct route {
 	GameState state;
-	struct route *next;           // Next element, if any
-	i64 len;                      // Number of inputs
-	u8 input[MAX_LENGTH];         // Inputs composing the route
+	struct route *next;    // Next element, if any
+	i64 score;             // How good the route looks (lower is better)
+	i64 length;            // Number of inputs
+	u8 input[MAX_LENGTH];  // Inputs composing the route
 } Route;
 
-static Route* routes[MAX_SCORE];      // Priority queue of routes to explore
-static i32 cur_score = MAX_SCORE - 1; // Index inside the queue
-static i32 best_score;
-static i64 best_len = MAX_LENGTH;     // Length of the shortest winning route
+// Memory pool of routes to explore
+static Route routes[BUF_SIZE];
+
+// Priority queue of pointers into the pool, ordered by fitness
+static Route* queue[MAX_LENGTH];
+static i32 cur_score = MAX_LENGTH;
+
+// Don’t explore routes that exceed those thresholds
+static volatile i64 length_cutoff = MAX_LENGTH;
+static i32 score_cutoff = MAX_LENGTH;
+
 static GameState initial_state;
-static u64 queued_routes;
 static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+static u64 explored_routes;
 
 // Returns a human-readable representation of a route
 static char* prettify_route(const Route *route)
@@ -30,64 +39,66 @@ static char* prettify_route(const Route *route)
 	static const char* symbols[] = {"←", "↓", "→", "↑", "s", "z"};
 	static char buf[3 * MAX_LENGTH + 1];
 
-	sprintf(buf, "%lu ", route->len);
-	for (i64 i = 0; i < route->len; ++i)
+	sprintf(buf, "%lu ", route->length);
+	for (i64 i = 0; i < route->length; ++i)
 		strcat(buf, symbols[route->input[i]]);
 	return buf;
-}
-
-static void shrink_queue()
-{
-	static i32 score = MAX_SCORE;
-	Route *removed;
-
-	pthread_mutex_lock(&mutex);
-	removed = routes[--score];
-	best_score = min(best_score, score - MAX_BACKTRACK);
-	routes[score] = NULL;
-	pthread_mutex_unlock(&mutex);
-
-	while (removed != NULL) {
-		--queued_routes;
-		free(removed);
-		removed = removed->next;
-	}
 }
 
 // Appends the given route to the priority queue
 static void add_to_queue(Route *route, i32 score)
 {
-	Route *new = malloc(sizeof(*new));
-	*new = *route;
+	static u32 i;
+	Route *new;
+
+	// Loop over the memory pool until we find a free slot
+	pthread_mutex_lock(&mutex);
+	do {
+		new = &routes[i++];
+		i %= BUF_SIZE;
+
+		// Avoid looping indefinitely when the pool is full
+		if (i == 0) {
+			--score_cutoff;
+			printf(YELLOW "cutoff\n" WHITE);
+		}
+
+	} while (new->length && new->score < score_cutoff);
+	pthread_mutex_unlock(&mutex);
+
+	if (score >= score_cutoff)
+		return;
+
+	new->length = route->length;
+	memcpy(new->input, route->input, MAX_LENGTH);
+	new->score = score;
 	new->state = g;
 
 	pthread_mutex_lock(&mutex);
-	new->next = routes[score];
-	routes[score] = new;
+	new->next = queue[score];
+	queue[score] = new;
 	cur_score = min(cur_score, score);
-	++queued_routes;
 	pthread_mutex_unlock(&mutex);
-
-	while (queued_routes > 65536)
-		shrink_queue();
 }
 
 // Removes the best route from the priority queue and returns it
 static Route* pop_queue()
 {
+	Route *result = NULL;
+
 	pthread_mutex_lock(&mutex);
-	while (routes[cur_score] == NULL)
-		if (++cur_score >= MAX_SCORE)
-			exit(0);
-	Route *result = routes[cur_score];
-	routes[cur_score] = result->next;
-	--queued_routes;
+	while (queue[cur_score] == NULL && cur_score < score_cutoff)
+		++cur_score;
+	if (cur_score < score_cutoff) {
+		result = queue[cur_score];
+		queue[cur_score] = result->next;
+	}
 	pthread_mutex_unlock(&mutex);
 
 	return result;
 }
 
-// lower is better
+// Computes the score of a route, based on the current game state
 static i32 fitness_function() {
 	if (player.hp <= 0)
 		return 255;
@@ -104,47 +115,51 @@ static void handle_victory(Route *route)
 	for (u32 i = 1; i <= 256 && (ok + 2) * 4 >= i; ++i) {
 		g = initial_state;
 		g.seed = i;
-		for (i64 beat = 0; beat < route->len && player.hp > 0; ++beat)
+		for (i64 beat = 0; beat < route->length && player.hp > 0; ++beat)
 			do_beat(route->input[beat]);
 		ok += player_won();
 	}
 
 	if (ok < 64)
 		return;
-	best_len = min(best_len, route->len);
+	length_cutoff = min(length_cutoff, route->length);
 	printf("%s\t(%2.1f%%)\n", prettify_route(route), ok / 2.56);
 }
 
 // Starts with the given route, then tries all possible inputs
 static void explore(Route *route)
 {
-	// Don’t explore routes that cannot beat the current best
-	if (route->len >= best_len)
-		return;
+	++route->length;
+	++explored_routes;
 
-	++route->len;
+	if (route->length > length_cutoff || route->score > score_cutoff)
+		return;
 
 	// Try adding each possible input at the end
 	for (u8 i = 0; i < 6; ++i) {
-		route->input[route->len - 1] = i;
+		route->input[route->length - 1] = i;
 		g = route->state;
 		do_beat(i);
 		i32 score = fitness_function();
 		assert(score >= 0);
-		best_score = min(best_score, score);
+		score_cutoff = min(score_cutoff, fitness_function() + MAX_BACKTRACK);
+
 		if (player_won())
 			handle_victory(route);
-		else if (score < best_score + MAX_BACKTRACK)
+		else if (score < score_cutoff)
 			add_to_queue(route, (u16) score);
 	}
 
-	free(route);
+	// Mark the route as done
+	route->length = 0;
 }
 
-static void* thread()
+static void* run()
 {
-	for (;;)
-		explore(pop_queue());
+	Route *route;
+	while ((route = pop_queue()))
+		explore(route);
+	return NULL;
 }
 
 // `solve` entry point: solves the dungeon
@@ -152,17 +167,16 @@ int main(i32 argc, char **argv)
 {
 	xml_parse(argc, argv);
 	do_beat(6);
-
 	g.seed = 0;
+
 	initial_state = g;
-	best_score = fitness_function();
+	routes[0].state = g;
+	explore(&routes[0]);
 
-	Route *route = calloc(1, sizeof(*route));
-	route->state = initial_state;
-	explore(route);
+	pthread_t child;
+	for (i64 i = 1; i < JOBS; ++i)
+		pthread_create(&child, NULL, run, NULL);
+	run();
 
-	pthread_t children;
-	for (i64 i = 0; i < JOBS; ++i)
-		pthread_create(&children, NULL, thread, NULL);
-	pthread_exit(NULL);
+	printf("%lu\n", explored_routes);
 }
