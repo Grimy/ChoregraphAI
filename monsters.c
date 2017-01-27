c792be00f0f3c1683632d4bdaf53261d608f282d
Victor Adam <victor@drawall.cc>
2017-01-27 21:13:10 +0100

    Purple slimes

--- monsters.c
+++ monsters.c
@@ -270,7 +270,7 @@ static void slime(Monster *this, __attribute__((unused)) Coords d)
 		{{ 0, -1}, { 0, 1}, { 0, -1}, { 0,  1}}, // blue
 		{{ 1,  0}, { 0, 1}, {-1,  0}, { 0, -1}}, // yolo
 		{{ 0,  0}, { 0, 0}, { 0,  0}, { 0,  0}}, // unassigned
-		{{ 1,  0}, {-1, 1}, { 1,  0}, {-1, -1}}, // Z
+		{{-1,  1}, { 1, 0}, {-1, -1}, { 1,  0}}, // Z
 		{{-1,  1}, { 1, 1}, { 1, -1}, {-1, -1}}, // fire
 		{{ 1,  1}, {-1, 1}, {-1, -1}, { 1, -1}}, // ice
 	};
@@ -687,7 +687,7 @@ const ClassInfos class_infos[256] = {
 	[ORC_3]       = {  3, 3, 0,   9, false, -1, 10303104, PURPLE "o", nop },
 	[DEVIL_1]     = {  2, 1, 2,   9, false, -1, 10201303, RED "&",    devil },
 	[DEVIL_2]     = {  4, 2, 2,   9, false, -1, 10402305, GREEN "&",  devil },
-	[Z_SLIME]     = {  3, 1, 0, 999, false, -1, 10301102, PURPLE "P", nop },
+	[Z_SLIME]     = {  3, 1, 0, 999, false, -1, 10301102, PURPLE "P", slime },
 	[CURSE]       = {  0, 1, 0,   9,  true, -1, 10001102, YELLOW "W", basic_seek },
 	[SHOP_MIMIC]  = {  2, 1, 0,   0, false, -1, 10201100, YELLOW "m", nop },
 
